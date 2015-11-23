817915b557c3b411e350b0f48bbab6327f501daf
Leanne Lee <leannelee812@berkeley.edu>
2015-11-23 13:37:47 -0800

    changed male and female csv

--- smoke.r
+++ smoke.r
@@ -97,6 +97,16 @@ ggplot(smoke_cancer_df) +
   
 # We see that in general, states with higher percentage of smokers have greater number of lung cancer patients
 
-male = read.delim("lung_cancer_male.txt")
-write.table(male, file="lung_cancer_male.csv",sep=",",col.names=TRUE,row.names=FALSE)
-male_df <- read.csv("lung_cancer_male.csv", header = TRUE, sep = ",")
+
+download.file(url = "https://raw.githubusercontent.com/leanne8/smoke_and_die/master/lung_cancer_male.txt",
+              destfile = "lung_cancer_male.csv")
+male_df <- read.csv("lung_cancer_male.csv", header = TRUE, sep = "\t")
+male_df[ , 1] <- NULL
+
+download.file(url = "https://raw.githubusercontent.com/leanne8/smoke_and_die/master/lung_cacner_%20female.txt",
+              destfile = "lung_cancer_female.csv")
+female_df <- read.csv("lung_cancer_female.csv", header = TRUE, sep = "\t")
+female_df[ , 1] <- NULL
+
+male_df[male_df == "~"] <- NA
+female_df[female_df == "~"] <- NA
