ec437f8c4346910763c45e71fe7b506989efee4f
Leanne Lee <leannelee812@berkeley.edu>
2015-12-08 18:42:49 -0800

    fix NV

--- code/smoke.r
+++ code/smoke.r
@@ -15,10 +15,12 @@ smoke_df <- smoke_df[ , c(1, 2)]
 colnames(smoke_df) <- c("state", "smokers(%)")
 
 lung_cancer_df <- lung_cancer_df[ , c(1, 3)]
-lung_cancer_df[ , 2] <- as.numeric(lung_cancer_df[ , 2])
-lung_cancer_df[lung_cancer_df$state == "NV", 2] <- round((1683/2685000) * 100000, digits = 1)
-lung_cancer_df[ , 2] <- lung_cancer_df[ , 2] / 1000
 colnames(lung_cancer_df) <- c("state", "cancer(%)")
+lung_cancer_df[ , 2] <- as.numeric(lung_cancer_df[ , 2])
+lung_cancer_df[lung_cancer_df$state == "NV", 2] <- 
+  round((1683/2685000) * 100000, digits = 1)
+lung_cancer_df[ , 2] <- lung_cancer_df[ , 2] / 1000
+
 
 smoke_cancer_df <- cbind(smoke_df, lung_cancer_df)
 smoke_cancer_df[ , 3] <- NULL 
@@ -83,12 +85,18 @@ barplot(sort(lung_cancer_perc),
 
 ## ---- chunk11 ----
 ggplot(smoke_cancer_cdf) +
-  geom_point(aes(x = names(smoker_perc), y = smoke_cancer_cdf$smokers..., col = "red")) +
-  geom_line(aes(x = names(smoker_perc), y = smoke_cancer_cdf$smokers..., col = "red", group = 1)) +
-  geom_point(aes(x = names(smoker_perc), y = smoke_cancer_cdf$cancer... * 1000, col = "green")) +
-  geom_line(aes(x = names(smoker_perc), y = smoke_cancer_cdf$cancer... * 1000, col = "green", group = 2)) +
+  geom_point(aes(x = names(smoker_perc), 
+                 y = smoke_cancer_cdf$smokers..., col = "red")) +
+  geom_line(aes(x = names(smoker_perc), 
+                y = smoke_cancer_cdf$smokers..., col = "red", group = 1)) +
+  geom_point(aes(x = names(smoker_perc), 
+                 y = smoke_cancer_cdf$cancer... * 1000, col = "green")) +
+  geom_line(aes(x = names(smoker_perc), 
+                y = smoke_cancer_cdf$cancer... * 1000, col = "green", group = 2)) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-  scale_color_manual(values = c("#AA3939", "#73DB1D"), name = "type", labels = c("lung cancer patients per 100000", "percentage of smokers")) +
+  scale_color_manual(values = c("#AA3939", "#73DB1D"), name = "type", 
+                     labels = c("lung cancer patients per 100000", 
+                                "percentage of smokers")) +
   theme(legend.position = "top") +
   xlab("States in the US") +
   ylab("")
@@ -100,7 +108,8 @@ smoke_cancer_fit
 plot(smoke_cancer_cdf$smokers..., smoke_cancer_cdf$cancer..., 
      pch = 16, cex = 1.3, col = "#063BB6",
      main = "cigarette smoking population -vs- lung cancer patients", las = 2, 
-     xlab = "number of cigarrete smokers per 100,000", ylab = "number of lung cancer patients per 100,000")
+     xlab = "number of cigarrete smokers per 100,000", 
+     ylab = "number of lung cancer patients per 100,000")
 abline(smoke_cancer_fit, col = "#FF0000", lwd = 2)
 
 ## ---- chunk13 ----
@@ -111,14 +120,19 @@ download.file(url = "https://raw.githubusercontent.com/leanne8/smoke_and_die/mas
               destfile = "../rawdata/lung_cancer_female.csv")
 
 ## ---- chunk14 ----
-male_df <- read.csv("../rawdata/lung_cancer_male.csv", header = TRUE, sep = "\t", 
-                    col.names = c("X", "male_age", "male_all", "white", "black", "asian", 
-                                  "native_american", "hispanic"), stringsAsFactors = FALSE)
+male_df <- read.csv("../rawdata/lung_cancer_male.csv", header = TRUE, 
+                    sep = "\t", col.names = c("X", "male_age", "male_all", 
+                                              "white", "black", "asian", 
+                                              "native_american", "hispanic"), 
+                    stringsAsFactors = FALSE)
 male_df[ , 1] <- NULL
 
-female_df <- read.csv("../rawdata/lung_cancer_female.csv", header = TRUE, sep = "\t",
-                      col.names = c("X", "female_age", "female_all", "white", "black", "asian", 
-                                    "native_american", "hispanic"), stringsAsFactors = FALSE)
+female_df <- read.csv("../rawdata/lung_cancer_female.csv", header = TRUE, 
+                      sep = "\t", col.names = c("X", "female_age", 
+                                                "female_all", "white", "black", 
+                                                "asian", "native_american", 
+                                                "hispanic"), 
+                      stringsAsFactors = FALSE)
 female_df[ , 1] <- NULL
 
 male_df[male_df == "~"] <- NA
@@ -157,7 +171,8 @@ for (i in 1:5) {
 names(total_rate_female) <- colnames(female_fortyfive_df)[3:7]
 
 ## ---- chunk16 ----
-total_rate_combined <- cbind("Male" = total_rate_male, "Female" = total_rate_female)
+total_rate_combined <- cbind("Male" = total_rate_male, 
+                             "Female" = total_rate_female)
 barplot(total_rate_combined, 
         col = c("#FFFFFF", "#000000", "#984126", "#FFFF00", "#E5A470"), 
         main = "Lung Cancer Patients by Race", 
@@ -165,7 +180,8 @@ barplot(total_rate_combined,
 legend("topright", 
        title = "Race",
        legend = c("white", "black", "native american", "asian", "hispanic"), 
-       fill = c("#FFFFFF", "#000000", "#984126", "#FFFF00", "#E5A470"), cex = 0.8)
+       fill = c("#FFFFFF", "#000000", "#984126", "#FFFF00", "#E5A470"), 
+       cex = 0.8)
 
 ## ---- chunk17 ----
 both_gender_df <- cbind(male_cdf, female_cdf)
@@ -183,26 +199,36 @@ ggplot (both_gender_df) +
 png(filename = "../images/smokepop.png")
 barplot(sort(smoker_perc), main = "Smoker Population in USA by State", 
         cex.names = 0.6, las = 2, ylab = "percentage of smokers",
-        col=c(rgb(255,20,147, maxColorValue = 255),rgb(30,144,254, maxColorValue = 255),
-              rgb(254,215,0, maxColorValue = 255), rgb(0,254,0, maxColorValue = 255)))
+        col=c(rgb(255,20,147, maxColorValue = 255),
+              rgb(30,144,254, maxColorValue = 255),
+              rgb(254,215,0, maxColorValue = 255), 
+              rgb(0,254,0, maxColorValue = 255)))
 dev.off()
 
 png(filename = "../images/lungcancer.png")
 barplot(sort(lung_cancer_perc), 
         main = "Lung Cancer Patients in USA by State", 
         cex.names = 0.6, las = 2, ylab = "percentage of lung cancer patients",
-        col=c(rgb(255,255,200, maxColorValue = 255),rgb(221,160, 221, maxColorValue = 255),
-              rgb(255,250,205, maxColorValue = 255), rgb(230,230,250, maxColorValue = 255)))
+        col=c(rgb(255,255,200, maxColorValue = 255),
+              rgb(221,160, 221, maxColorValue = 255),
+              rgb(255,250,205, maxColorValue = 255), 
+              rgb(230,230,250, maxColorValue = 255)))
 dev.off()
 
 png(filename = "../images/comp.png")
 figure1 <- ggplot(smoke_cancer_cdf) +
-  geom_point(aes(x = names(smoker_perc), y = smoke_cancer_cdf$smokers..., col = "red")) +
-  geom_line(aes(x = names(smoker_perc), y = smoke_cancer_cdf$smokers..., col = "red", group = 1)) +
-  geom_point(aes(x = names(smoker_perc), y = smoke_cancer_cdf$cancer... * 1000, col = "green")) +
-  geom_line(aes(x = names(smoker_perc), y = smoke_cancer_cdf$cancer... * 1000, col = "green", group = 2)) +
+  geom_point(aes(x = names(smoker_perc), 
+                 y = smoke_cancer_cdf$smokers..., col = "red")) +
+  geom_line(aes(x = names(smoker_perc), 
+                y = smoke_cancer_cdf$smokers..., col = "red", group = 1)) +
+  geom_point(aes(x = names(smoker_perc),
+                 y = smoke_cancer_cdf$cancer... * 1000, col = "green")) +
+  geom_line(aes(x = names(smoker_perc), 
+                y = smoke_cancer_cdf$cancer... * 1000, col = "green", group = 2)) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-  scale_color_manual(values = c("#AA3939", "#73DB1D"), name = "type", labels = c("lung cancer patients per 100000", "percentage of smokers")) +
+  scale_color_manual(values = c("#AA3939", "#73DB1D"), name = "type", 
+                     labels = c("lung cancer patients per 100000", 
+                                "percentage of smokers")) +
   theme(legend.position = "top") +
   xlab("States in the US") +
   ylab("")
@@ -213,17 +239,21 @@ png(filename = "../images/reg.png")
 plot(smoke_cancer_cdf$smokers..., smoke_cancer_cdf$cancer..., 
      pch = 16, cex = 1.3, col = "#063BB6",
      main = "cigartte smoking population -vs- lung cancer patients", las = 2, 
-     xlab = "number of cigarette smokers per 100,000", ylab = "number of lung cancer patients per 100,000") +
+     xlab = "number of cigarette smokers per 100,000", 
+     ylab = "number of lung cancer patients per 100,000") +
   abline(smoke_cancer_fit, col = "#FF0000", lwd = 2)
 dev.off()
 
 png(filename = "../images/race.png")
-barplot(total_rate_combined, col = c("#FFFFFF", "#000000", "#984126", "#FFFF00", "#E5A470"), 
-        main = "Lung Cancer Patients by Race", ylab = "frequency per 100,000", beside = TRUE)
+barplot(total_rate_combined, col = c("#FFFFFF", "#000000", "#984126", "#FFFF00",
+                                     "#E5A470"), 
+        main = "Lung Cancer Patients by Race", 
+        ylab = "frequency per 100,000", beside = TRUE)
 legend("topright", 
        title = "Race",
        legend = c("white", "black", "native american", "asian", "hispanic"), 
-       fill = c("#FFFFFF", "#000000", "#984126", "#FFFF00", "#E5A470"), cex = 0.8)
+       fill = c("#FFFFFF", "#000000", "#984126", "#FFFF00", "#E5A470"), 
+       cex = 0.8)
 dev.off()
 
 png(filename = "../images/age.png")
