b48451e4117ad5aa98cae687380922c33126fabe
tony1993 <yeonghwan.son@hotmail.com>
2015-12-08 20:03:15 -0800

    data dictionaries created

---
+++ rawdata/rawdata_download.R
@@ -0,0 +1,13 @@
+# Codes for Downloading Raw Data
+
+download.file(url = "https://raw.githubusercontent.com/leanne8/smoke_and_die/master/rawdata/smoke_df.csv", 
+              destfile = "smoke_df.csv")
+
+download.file(url = "https://raw.githubusercontent.com/leanne8/smoke_and_die/master/rawdata/lung_cancer_df.csv",
+              destfile = "lung_cancer_df.csv")
+
+download.file(url = "https://raw.githubusercontent.com/leanne8/smoke_and_die/master/rawdata/lung_cancer_male.txt",
+              destfile = "lung_cancer_male.csv")
+
+download.file(url = "https://raw.githubusercontent.com/leanne8/smoke_and_die/master/rawdata/lung_cacner_%20female.txt",
+              destfile = "lung_cancer_female.csv")
