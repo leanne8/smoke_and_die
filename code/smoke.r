f0717057c346536f113f95be6852b0b8cf30e9a8
tony1993 <yeonghwan.son@hotmail.com>
2015-12-08 16:21:44 -0800

    image files created

--- code/smoke.r
+++ code/smoke.r
@@ -41,9 +41,8 @@ smoke_cancer_df[ , 3] <- NULL
 
 write.table(smoke_cancer_df, file = "../data/smoke_cancer_cdf.csv", row.names = FALSE, col.names = TRUE, sep = ",")
 
-smoke_cancer_cdf <- read.csv(file = "../data/smoke_cancer_cdf.csv", header = TRUE, stringsAsFactors = FALSE)
-
 ## ---- chunk6 ----
+smoke_cancer_cdf <- read.csv(file = "../data/smoke_cancer_cdf.csv", header = TRUE, stringsAsFactors = FALSE)
 smoke_cancer_cdf$state[which.max(smoke_cancer_cdf$smokers...)]
 
 ## ---- chunk7 ----
@@ -79,7 +78,7 @@ ggplot(smoke_cancer_cdf) +
   geom_point(aes(x = names(smoker_perc), y = smoke_cancer_cdf$cancer... * 1000, col = "green")) +
   geom_line(aes(x = names(smoker_perc), y = smoke_cancer_cdf$cancer... * 1000, col = "green", group = 2)) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-  scale_color_manual(values = c("#AA3939", "#73DB1D"), name = "type", labels = c("percentage of smokers", "number of lung cancer patients per 100000")) +
+  scale_color_manual(values = c("#AA3939", "#73DB1D"), name = "type", labels = c("lung cancer patients per 100000", "percentage of smokers")) +
   theme(legend.position = "top") +
   xlab("States in the US") +
   ylab("")
@@ -90,7 +89,7 @@ smoke_cancer_fit
 
 plot(smoke_cancer_cdf$smokers..., smoke_cancer_cdf$cancer..., 
      pch = 16, cex = 1.3, col = "#063BB6",
-     main = "cigartte smoking population -vs- lung cancer patients", las = 2, 
+     main = "cigarette smoking population -vs- lung cancer patients", las = 2, 
      xlab = "number of cigarrete smokers per 100,000", ylab = "number of lung cancer patients per 100,000")
 abline(smoke_cancer_fit, col = "#FF0000", lwd = 2)
 
@@ -161,8 +160,69 @@ ggplot (both_gender_df) +
   geom_bar(aes(x = both_gender_df$female_age, y = both_gender_df$female_all), 
            stat = "identity", col= "#FF6699") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
-  xlab("Age") + ylab("Number of patients with lung cancer per 100,000") +
+  xlab("Age") + ylab("lung cancer patients per 100,000") +
   ggtitle("Lung Cancer Patients by Age")
+
+## ---- chunk19 ----
+png(filename = "../images/smokepop.png")
+barplot(sort(smoker_perc), main = "Smoker Population in USA by State", 
+        cex.names = 0.6, las = 2, ylab = "percentage of smokers",
+        col=c(rgb(255,20,147, maxColorValue = 255),rgb(30,144,254, maxColorValue = 255),
+              rgb(254,215,0, maxColorValue = 255), rgb(0,254,0, maxColorValue = 255)))
+dev.off()
+
+png(filename = "../images/lungcancer.png")
+barplot(sort(lung_cancer_perc), 
+        main = "Lung Cancer Patients in USA by State", 
+        cex.names = 0.6, las = 2, ylab = "percentage of smokers",
+        col=c(rgb(255,255,200, maxColorValue = 255),rgb(221,160, 221, maxColorValue = 255),
+              rgb(255,250,205, maxColorValue = 255), rgb(230,230,250, maxColorValue = 255)))
+dev.off()
+
+png(filename = "../images/comp.png")
+figure1 <- ggplot(smoke_cancer_cdf) +
+  geom_point(aes(x = names(smoker_perc), y = smoke_cancer_cdf$smokers..., col = "red")) +
+  geom_line(aes(x = names(smoker_perc), y = smoke_cancer_cdf$smokers..., col = "red", group = 1)) +
+  geom_point(aes(x = names(smoker_perc), y = smoke_cancer_cdf$cancer... * 1000, col = "green")) +
+  geom_line(aes(x = names(smoker_perc), y = smoke_cancer_cdf$cancer... * 1000, col = "green", group = 2)) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_color_manual(values = c("#AA3939", "#73DB1D"), name = "type", labels = c("lung cancer patients per 100000", "percentage of smokers")) +
+  theme(legend.position = "top") +
+  xlab("States in the US") +
+  ylab("")
+plot(figure1)
+dev.off()
+
+png(filename = "../images/reg.png")
+plot(smoke_cancer_cdf$smokers..., smoke_cancer_cdf$cancer..., 
+     pch = 16, cex = 1.3, col = "#063BB6",
+     main = "cigartte smoking population -vs- lung cancer patients", las = 2, 
+     xlab = "number of cigarette smokers per 100,000", ylab = "number of lung cancer patients per 100,000") +
+  abline(smoke_cancer_fit, col = "#FF0000", lwd = 2)
+dev.off()
+
+png(filename = "../images/race.png")
+barplot(total_rate_combined, col = c("#FFFFFF", "#000000", "#984126", "#FFFF00", "#E5A470"), 
+        main = "Lung Cancer Patients by Race", ylab = "frequency per 100,000", beside = TRUE)
+legend("topright", 
+       title = "Race",
+       legend = c("white", "black", "native american", "asian", "hispanic"), 
+       fill = c("#FFFFFF", "#000000", "#984126", "#FFFF00", "#E5A470"), cex = 0.8)
+dev.off()
+
+png(filename = "../images/age.png")
+figure2 <- ggplot (both_gender_df) +
+  geom_bar(aes(x = both_gender_df$male_age, y = both_gender_df$male_all), 
+           stat = "identity", col= "#0033CC") +
+  geom_bar(aes(x = both_gender_df$female_age, y = both_gender_df$female_all), 
+           stat = "identity", col= "#FF6699") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+  xlab("Age") + ylab("lung cancer patients per 100,000") +
+  ggtitle("Lung Cancer Patients by Age")
+plot(figure2)
+dev.off()
+
+# -------------- The End -----------------
 
 ## We first look at the population of cigarette smokers and those diagnosed with lung cancer in each state. 
 
@@ -342,8 +402,3 @@ ggplot (both_gender_df) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
   xlab("Age") + ylab("Number of patients with lung cancer per 100,000") +
   ggtitle("Lung Cancer Patients by Age")
-
-# Creating clean data table from the raw data table
-write.table(smoke_cancer_df, file = "../data/smoke_cancer_cdf.csv", row.names = FALSE, col.names = TRUE, sep = ",")
-write.table(male_df, file = "../data/male_cdf.csv", row.names = FALSE, col.names = TRUE, sep = ",")
-write.table(female_df, file = "../data/female_cdf.csv", row.names = FALSE, col.names = TRUE, sep = ",")
